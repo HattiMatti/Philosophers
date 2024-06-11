2af23cb9443255d9eca8110a39271de20ab7003e
Matti Siitonen <msiitone@c1r4p2.hive.fi>
2024-06-11 18:18:17 +0300

    put printf inside the if statement

--- srcs/main.c
+++ srcs/main.c
@@ -25,12 +25,10 @@ int	main(int argc, char **argv)
 		args.time_to_sleep = ft_atoi(argv[4]);
 		if (argc == 6)
 			args.nbr_of_meals = ft_atoi(argv[5]);
+		printf("%d, %d, %d, %d, %d", args.nbr_of_philo, args.time_to_die,
+			args.time_to_eat, args.time_to_sleep, args.nbr_of_meals);
 	}
 	else
-	{
 		ft_putstr_fd("Error: wrong number of arguments", 2);
-	}
-	printf("%d, %d, %d, %d, %d", args.nbr_of_philo, args.time_to_die,
-		args.time_to_eat, args.time_to_sleep, args.nbr_of_meals);
 	return (0);
 }
